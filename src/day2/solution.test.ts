dc0bd58a1585c4f771ae0cfecb8f9e0af152bfaf
Martijn <mjsboom@gmail.com>
2021-12-02 21:12:51 +0100

    Split exec into seperate functions. Removed backward movement from switch

--- src/day2/solution.test.ts
+++ src/day2/solution.test.ts
@@ -1,12 +1,12 @@
-import { exec } from "./solution";
+import { part0, part1 } from "./solution";
 import * as path from "path";
 
 test("first example equal to 150", () => {
   const data_path = path.join(__dirname, "sample_data.txt");
-  expect(exec(0, data_path) == 150);
+  expect(part0(data_path) == 150);
 });
 
 test("second example equal to 900", () => {
   const data_path = path.join(__dirname, "sample_data.txt");
-  expect(exec(1, data_path) == 900);
+  expect(part1(data_path) == 900);
 });
--- src/day2/solution.ts
+++ src/day2/solution.ts
@@ -1,7 +1,16 @@
 import * as fs from "fs";
 import * as path from "path";
 
-export function exec(part: number, data_path: string = "") {
+export function part0(data_path: string = "") {
+  let { hor, aim } = solve(read_input(data_path));
+  return hor * aim;
+}
+export function part1(data_path: string = "") {
+  let { hor, ver } = solve(read_input(data_path));
+  return hor * ver;
+}
+
+function read_input(data_path: string = "") {
   if (!data_path) data_path = path.join(__dirname, "data.txt");
   const input: [string, number][] = fs
     .readFileSync(data_path)
@@ -9,7 +18,10 @@ export function exec(part: number, data_path: string = "") {
     .split("\n")
     .map((x, _) => x.split(" "))
     .map((x, _) => [x[0], parseInt(x[1])]);
+  return input;
+}
 
+function solve(input: [string, number][]) {
   let hor = 0;
   let ver = 0;
   let aim = 0;
@@ -21,10 +33,6 @@ export function exec(part: number, data_path: string = "") {
         hor += length;
         ver += aim * length;
         break;
-      case "backward":
-        hor -= length;
-        ver -= aim * length;
-        break;
       case "up":
         aim -= length;
         break;
@@ -34,6 +42,5 @@ export function exec(part: number, data_path: string = "") {
     }
   }
 
-  if (part == 0) return aim * ver;
-  else return hor * ver;
+  return { hor, ver, aim };
 }
