171526d1d6dd0df44c8f423e92378d1817dbc3ff
Martijn <mjsboom@gmail.com>
2021-12-02 07:22:20 +0100

    fixed day2 test names to correct expected values

--- src/day2/solution.test.ts
+++ src/day2/solution.test.ts
@@ -1,12 +1,12 @@
 import { exec } from "./solution";
 import * as path from "path";
 
-test("first example equal to 7", () => {
+test("first example equal to 150", () => {
   const data_path = path.join(__dirname, "sample_data.txt");
   expect(exec(0, data_path) == 150);
 });
 
-test("second example equal to 5", () => {
+test("second example equal to 900", () => {
   const data_path = path.join(__dirname, "sample_data.txt");
   expect(exec(1, data_path) == 900);
 });
