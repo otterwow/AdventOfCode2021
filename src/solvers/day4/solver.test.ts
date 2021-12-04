c0c64e5d697a1d7bb5d6b2d3dbc9b41203601c27
Martijn <mjsboom@gmail.com>
2021-12-04 17:42:26 +0100

    corrected value for day4 test

--- src/solvers/day4/solver.test.ts
+++ src/solvers/day4/solver.test.ts
@@ -3,10 +3,10 @@ import { read_data } from "../../solver_manager";
 
 test("Part 1 correct for sample data", () => {
   const data = read_data(4, true);
-  expect(new Day4Solver(data).part_1()).toBe(-1);
+  expect(new Day4Solver(data).part_1()).toBe(4512);
 });
 
-// test("Part 2 correct for sample data", () => {
-//   const data = read_data(4, true);
-//   expect(new Day4Solver(data).part_2()).toBe(-1);
-// });
+test("Part 2 correct for sample data", () => {
+  const data = read_data(4, true);
+  expect(new Day4Solver(data).part_2()).toBe(2980);
+});
