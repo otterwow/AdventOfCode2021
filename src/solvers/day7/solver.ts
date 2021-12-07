1b1f1aa5dd13cb25337c0052cdfb5269ed89a630
Martijn <mjsboom@gmail.com>
2021-12-07 18:18:45 +0100

    change finding optimal x-location to mean/median

--- src/solvers/day7/solver.ts
+++ src/solvers/day7/solver.ts
@@ -1,4 +1,5 @@
 import Solver from "../../solver_interface";
+import { mean, median, compose } from "ramda";
 
 export default class Day7Solver extends Solver {
   constructor(data: string[]) {
@@ -10,23 +11,22 @@ export default class Day7Solver extends Solver {
   }
 
   part_1() {
-    return this.solve((x: number, y: number) => Math.abs(x - y));
+    return this.solve((x: number, y: number) => Math.abs(x - y), median);
   }
 
   part_2() {
-    return this.solve(
-      (x: number, y: number) => (Math.abs(x - y) * (Math.abs(x - y) + 1)) / 2
+    const distance_func = (x: number, y: number) =>
+      Math.abs(x - y) * (Math.abs(x - y) + 1);
+    return (
+      Math.min(
+        this.solve(distance_func, compose(Math.ceil, mean)),
+        this.solve(distance_func, compose(Math.floor, mean))
+      ) / 2
     );
   }
 
-  solve(distance_func: any) {
-    let optimal_fuel_cost = Number.MAX_VALUE;
-    for (let i = Math.min(...this.data); i < Math.max(...this.data); i++) {
-      optimal_fuel_cost = Math.min(
-        optimal_fuel_cost,
-        this.data.reduce((acc, x) => acc + distance_func(x, i), 0)
-      );
-    }
-    return optimal_fuel_cost;
+  solve(distance_func: any, reduction_func: any) {
+    const optimal_x = reduction_func(this.data);
+    return this.data.reduce((acc, x) => acc + distance_func(x, optimal_x), 0);
   }
 }
