6d8e13098cf0f87bed4743f30558a0eaeb0f8298
Martijn <mjsboom@gmail.com>
2021-12-10 08:49:11 +0100

    small clean up for day 10

--- src/solvers/day10/solver.ts
+++ src/solvers/day10/solver.ts
@@ -1,24 +1,26 @@
-import { contains, last, median, reverse, sum, slice } from "ramda";
+import { contains, last, median, reverse, sum } from "ramda";
 import Solver from "../../solver_interface";
 
 const opening_brackets = ["(", "[", "{", "<"];
 const closing_brackets = [")", "]", "}", ">"];
 
-const bracket_pairs = new Map();
-bracket_pairs.set(")", "(");
-bracket_pairs.set("]", "[");
-bracket_pairs.set("}", "{");
-bracket_pairs.set(">", "<");
+const bracket_pairs = new Map([
+  [")", "("],
+  ["]", "["],
+  ["}", "{"],
+  [">", "<"],
+]);
 
-const bracket_scores = new Map();
-bracket_scores.set("(", 1);
-bracket_scores.set("[", 2);
-bracket_scores.set("{", 3);
-bracket_scores.set("<", 4);
-bracket_scores.set(")", 3);
-bracket_scores.set("]", 57);
-bracket_scores.set("}", 1197);
-bracket_scores.set(">", 25137);
+const bracket_scores = new Map([
+  ["(", 1],
+  ["[", 2],
+  ["{", 3],
+  ["<", 4],
+  [")", 3],
+  ["]", 57],
+  ["}", 1197],
+  [">", 25137],
+]);
 
 export default class Day10Solver extends Solver {
   constructor(data: string[]) {
@@ -33,7 +35,7 @@ export default class Day10Solver extends Solver {
     return sum(
       this.solve()
         .filter((memory) => contains(last(memory), closing_brackets))
-        .map((memory) => bracket_scores.get(last(memory)))
+        .map((memory) => bracket_scores.get(last(memory)!)!)
     );
   }
 
@@ -44,24 +46,23 @@ export default class Day10Solver extends Solver {
         .map((memory: string[]) =>
           reverse(memory).reduce(
             (acc: number, bracket: string) =>
-              acc * 5 + bracket_scores.get(bracket),
+              acc * 5 + bracket_scores.get(bracket)!,
             0
           )
         )
     );
   }
 
-  solve() {
+  solve(): string[][] {
     return this.data.map((line) =>
       line.reduce(
-        (acc: string[], bracket: string, _: number, arr: string[]) => {
-          if (contains(bracket, opening_brackets)) return [...acc, bracket];
-          else if (bracket_pairs.get(bracket) == last(acc))
-            return slice(0, -1, acc);
-          else {
+        (stack: string[], bracket: string, _: number, arr: string[]) => {
+          if (contains(bracket, opening_brackets)) return [...stack, bracket];
+          if (bracket_pairs.get(bracket) != stack.pop()) {
             arr.splice(1);
-            return [...acc, bracket];
+            return [bracket];
           }
+          return stack;
         },
         []
       )
